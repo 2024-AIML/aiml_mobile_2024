48818d2986737d6be8dfce164399ac2df15ae0c5
Lazernes <dbsauddn1@gmail.com>
2024-04-30 12:46:21 +0900

    [Docs] infra_info Navermap API connect success

--- lib/screens/infra_info.dart
+++ lib/screens/infra_info.dart
@@ -1,51 +1,38 @@
-import 'dart:async';
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
-// import 'package:flutter_naver_map/flutter_naver_map.dart';
+import 'package:flutter_naver_map/flutter_naver_map.dart';
 
 void main() async {
-//  await _initialize();
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await NaverMapSdk.instance.initialize(clientId: '1dtmwihlto');
+  WidgetsFlutterBinding.ensureInitialized();
+  await NaverMapSdk.instance.initialize(clientId: '1dtmwihlto',
+  onAuthFailed: (ex) {
+    print("******네이버맵 인증 오류: $ex ******");
+  });
   runApp(const infra_info());
 }
-
-//지도 초기화하기
-// Future<void> _initialize() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await NaverMapSdk.instance.initialize(
-//     clientId: '1dtmwihlto',
-//     onAuthFailed: (e) => log("네이버맵 인증오류 : $e", name: "onAuthFailed")
-//   );
-// }
 
 class infra_info extends StatelessWidget {
   const infra_info({super.key});
 
   @override
   Widget build(BuildContext context) {
-
-//    final Completer<NaverMapController> mapControllerCompleter = Completer();
-
     return MaterialApp(
       title: 'infra_info',
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Color(0xffD9D9D9),
         ),
-        // body: NaverMap(
-        //    options: const NaverMapViewOptions(
-        //     indoorEnable: true,
-        //     locationButtonEnable: false,
-        //     consumeSymbolTapEvents: false,
-        //    ),
-        //   onMapReady: (controller) async {
-        //     mapControllerCompleter.complete(controller);
-        //     log("onMapReady", name: "onMapReady");
-        //     // print("로딩!");
-        //   },
-        // ),
+        body: NaverMap(
+           options: const NaverMapViewOptions(
+            // indoorEnable: true,
+            // locationButtonEnable: false,
+            // consumeSymbolTapEvents: false,
+           ),
+          onMapReady: (controller) async {
+            // mapControllerCompleter.complete(controller);
+            // log("onMapReady", name: "onMapReady");
+            print("로딩!");
+          },
+        ),
         bottomNavigationBar: BottomNavigationBar(
           type: BottomNavigationBarType.fixed,
           backgroundColor: Color(0xffD9D9D9),
